0fa1e2f1356f2aed85d9dc0c85dc9753c3e89ce3
Cippman <cipponessuno@gmail.com>
2022-04-11 20:16:21 +0200

    moved files to right repositories

--- Runtime/Delegates.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CippSharp.Core.Coroutines
-{
-    /// <summary>
-    /// Callback for when something is completed.
-    /// </summary>
-    /// <param name="success"></param>
-    public delegate void CompletedCallback(bool success);
-}
